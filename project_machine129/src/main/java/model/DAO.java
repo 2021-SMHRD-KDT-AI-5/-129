d6b37ff95c5ef3098ca19bb70c1d3e04509b68fc
김대원 <85005828+kdw6158@users.noreply.github.com>
2021-07-05 10:03:21 +0900

    0705 1000 프로젝트 통합

---
+++ project_machine129/src/main/java/model/DAO.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/DAO")
+public class DAO extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
